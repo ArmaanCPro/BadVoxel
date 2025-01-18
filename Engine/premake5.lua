91eee6c8c0be631359ba839180a67dee57e24ae9
Armaan Chahal <armaan.c2009@gmail.com>
2025-01-17 22:13:53 -0600

    vertex shader DONE. and they said I couldn't do it

--- Engine/premake5.lua
+++ Engine/premake5.lua
@@ -5,7 +5,7 @@ project "Engine"
    targetdir "bin/%{cfg.buildcfg}"
    staticruntime "off"
 
-   files { "src/**.h", "src/**.cpp" }
+   files { "src/**.h", "src/**.cpp", "shaders/**.glsl" }
 
    includedirs
    {
--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -4,6 +4,14 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
+// vertex shader source code. messy for now
+static const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+		"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -17,6 +25,7 @@ void processInput(GLFWwindow* window)
 
 int main()
 {
+	
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -48,11 +57,31 @@ int main()
 	 0.5f, -0.5f, 0.0f,
 	 0.0f,  0.5f, 0.0f };
 
+	// VBO bullshits
 	unsigned int VBO; // this is our actual VBO.
 	glGenBuffers(1, &VBO); // generate the buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO); // opengl has many types of buffer objects. vertex buffer object is GL_ARRAY_BUFFER
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // copies our vertices into the currently bound GL_ARRAY_BUFFER buffer (which is our VBO)
 
+	// vertex shader bullshits
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+
+	// temp test bullshits
+	{
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+	}
+
+	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// input
