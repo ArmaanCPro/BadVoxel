eb9f4e9ef5a2fbd9e6c44d96d14131773fdfb299
Armaan Chahal <armaan.c2009@gmail.com>
2025-01-18 01:26:09 -0600

    color = pos

--- Engine/shaders/vertex.glsl
+++ Engine/shaders/vertex.glsl
@@ -4,10 +4,14 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 
 out vec3 ourColor;
+out vec3 ourPos;
+
 uniform float uHorizOffset;
+
 
 void main()
 {
-    gl_Position = vec4(aPos.x + uHorizOffset, aPos.y * -1.0, aPos.z, 1.0);
+    ourPos = vec3(aPos.x + uHorizOffset, aPos.y * -1.0, aPos.z);
+    gl_Position = vec4(ourPos, 1.0);
     ourColor = aColor;
 }
