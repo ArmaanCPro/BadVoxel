1a0ac8d163a4430933377358cf90a9e9d1146f7e
Armaan Chahal <armaan.c2009@gmail.com>
2025-01-24 23:20:41 -0600

    refactor main function bullshits into game class

---
+++ Engine/src/game.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "Camera.h"
+#include "renderer.h"
+#include "window.h"
+
+namespace BV
+{
+    class game;
+}
+
+class BV::game
+{
+public:
+    game();
+    ~game();
+    void update_model();
+    void draw_frame();
+
+    window Window;
+    renderer Renderer;
+    Camera camera;
+private:
+    
+
+    // mouse bullshits
+    float lastX = 0.0f;
+    float lastY = 0.0f;
+    bool firstMouse = true;
+    float lastMouseX = 0.0f, lastMouseY = 0.0f;
+
+    // deltatime bullshits
+    float deltaTime = 0.0f; // time between cur frame and last frame
+    float lastFrame = 0.0f; // time of last frame
+
+protected:
+
+    void processInput(const BV::window& window);
+
+    static inline void APIENTRY OGL_DebugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
+                                 GLsizei length, const GLchar* message, const void* userParam)
+    {
+        std::cerr << "GL CALLBACK: " << (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "")
+                  << " type = " << type << ", severity = " << severity
+                  << ", message = " << message << std::endl;
+    }
+};
--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -10,177 +10,20 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "Camera.h"
+#include "game.h"
 #include "renderer.h"
 #include "Shader.h"
 #include "window.h"
 
-void processInput(const BV::window& windowIn);
-
-void APIENTRY OGL_DebugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity,
-                                 GLsizei length, const GLchar* message, const void* userParam)
-{
-    std::cerr << "GL CALLBACK: " << (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "")
-              << " type = " << type << ", severity = " << severity
-              << ", message = " << message << std::endl;
-}
-
-// screen settings
-constexpr unsigned int SCREEN_WIDTH = 800;
-constexpr unsigned int SCREEN_HEIGHT = 600;
-
-// camera statics
-glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
-
-// mouse bullshits
-float lastX = SCREEN_WIDTH / 2.0f;
-float lastY = SCREEN_HEIGHT / 2.0f;
-bool firstMouse = true;
-
-
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-float deltaTime = 0.0f; // time between cur frame and last frame
-float lastFrame = 0.0f; // time of last frame
-
-float lastMouseX = 0.0f, lastMouseY = 0.0f;
-
 int main()
 {
+	BV::game Game;
 
-	// window init
-	BV::window Window(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-#ifdef BV_DEBUG
-	glEnable(GL_DEBUG_OUTPUT);
-	glDebugMessageCallback(OGL_DebugMessageCallback, 0);
-#endif
-	
-	
-	// renderer & shader initialization bullshits
-	Shader shader("shaders/vertex.glsl", "shaders/fragment.glsl");
-	BV::renderer Renderer;
-	Renderer.init();
-	
-	Renderer.set_texture("shaders/mc_grass.jpg", GL_RGB);
-	
-	Window.AddMouseEvent([&](float xpos, float ypos)
+	while (!glfwWindowShouldClose(Game.Window.getGLFWwindow()))
 	{
-		if (firstMouse)
-		{
-			lastMouseX = xpos;
-			lastMouseY = ypos;
-			firstMouse = false;
-		}
-		float xoffset = xpos - lastMouseX;
-		float yoffset = lastMouseY - ypos; // reversed since y-coordinates go from bottom to top
-			
-		lastMouseX = xpos;
-		lastMouseY = ypos;
-			
-		camera.ProcessMouseMovement(xoffset, yoffset);
-	});
+		Game.update_model();
 
-	
-	float vertices[] = {
-            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-             0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-    
-            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-             0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-            -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-    
-            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-            -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-            -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    
-             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-             0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-             0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-    
-            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-             0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-             0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-            -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-            -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-    
-            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-             0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-            -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-            -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-	};
-
-	//Renderer.load_vertices(vertices, 180);
-	Renderer.add_cube(glm::vec3{0.0f, 0.0f, -10.0f});
-	// TODO: Only the 1 cube out of these two would be drawn, and it is the one called last
-	Renderer.add_cube(glm::vec3{0.0f, 5.0f, -10.0f});
-
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            Renderer.add_cube(glm::vec3{i * 3, 0.0f, j});
-        }
-    }
-	
-	// render loop
-	while (!glfwWindowShouldClose(Window.getGLFWwindow()))
-	{
-		// delta time bullshits
-		float currentFrame = (float)glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-		
-		// input
-		processInput(Window);
-		
-		// rendering commands here
-		auto scrDims = Window.getWindowDimensions();
-		Renderer.draw_vertices(camera, (float)scrDims.first, (float)scrDims.second, false);
-		
-		// check and call events and swap the buffers
-		glfwSwapBuffers(Window.getGLFWwindow());
-		glfwPollEvents();
+		Game.draw_frame();
 	}
-	
-	
-	Renderer.reset();
-	
 	return 0;
-}
-
-void processInput(const BV::window& windowIn)
-{
-	if (windowIn.isKeyPressed(GLFW_KEY_ESCAPE) == true)
-		glfwSetWindowShouldClose(windowIn.getGLFWwindow(), true);
-
-	if (windowIn.isKeyPressed(GLFW_KEY_W) == true)
-		camera.ProcessKeyboard(FORWARD, deltaTime);
-	if (windowIn.isKeyPressed(GLFW_KEY_S) == true)
-		camera.ProcessKeyboard(BACKWARD, deltaTime);
-	if (windowIn.isKeyPressed(GLFW_KEY_A) == true)
-		camera.ProcessKeyboard(LEFT, deltaTime);
-	if (windowIn.isKeyPressed(GLFW_KEY_D) == true)
-		camera.ProcessKeyboard(RIGHT, deltaTime);
-
-	if (windowIn.isKeyPressed(GLFW_KEY_SPACE) == true)
-		camera.ProcessKeyboard(UP, deltaTime);
-	if (windowIn.isKeyPressed(GLFW_KEY_LEFT_CONTROL) == true)
-		camera.ProcessKeyboard(DOWN, deltaTime);
 }
