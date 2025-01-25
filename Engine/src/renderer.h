a62e151e1aa9166b70f2e7beca648537abb1ee5d
Armaan Chahal <armaan.c2009@gmail.com>
2025-01-24 21:59:01 -0600

    CLEANUP CREW

--- Engine/src/renderer.h
+++ Engine/src/renderer.h
@@ -15,18 +15,33 @@ namespace BV
 class BV::renderer
 {
 public:
+    // make sure to call init() after constructing the object!
     renderer(std::string vsPath = "shaders/vertex.glsl", std::string fsPath = "shaders/fragment.glsl") : shader(vsPath.c_str(), fsPath.c_str()) {}
     ~renderer() { reset(); }
+    renderer(const renderer&) = delete;
+    renderer& operator=(const renderer&) = delete;
 
+    // initializes all OpenGL stuff. Constructor does not call this.
     void init();
+
+    // currently no rotation or size.
     void add_cube(const glm::vec3& position);
+    // Used internally but can also be used externally to load vertices into the buffer. Make sure the vertices are in the layout of 3 for the position and 2 for the texcoords.
     void load_vertices(const float* verts, size_t size);
+
+    // this is the final draw call. it should handle all opengl related rendering commands, so you shouldn't have those in your render loop.
     void draw_vertices(const Camera& camera, float screenWidth, float screenHeight, bool wireframe = false);
+
+    /* @params
+     * texturePath - this is the filepath to the texture.
+     * imageFormat - this is the RGBA format of the image. Generally only GL_RGBA or GL_RGB */
     void set_texture(const std::string& texturePath, unsigned int imageFormat = GL_RGBA);
+
+    // the reset function basically reverts all allocations. is called in the destructor
     void reset();
 private:
-    unsigned int VAO, VBO;
+    unsigned int VAO = 0, VBO = 0;
     Shader shader;
-    unsigned int textureID;
+    unsigned int textureID = 0;
     std::vector<float> vertices;
 };
--- Engine/src/window.h
+++ Engine/src/window.h
@@ -17,28 +17,28 @@ public:
 	window(const window&) = delete;
 	window& operator=(const window&) = delete;
 
-	// for debug purposes, until all GLFW window functions can be abstracted
+	// for debug purposes, until all needed GLFW window functions can be abstracted
 	GLFWwindow* getGLFWwindow() const { return m_window; }
 
+	// returns if a key is pressed. should be used for input processing.
 	bool isKeyPressed(int keycode) { return glfwGetKey(m_window, keycode) == GLFW_PRESS; }
 	// width, height
 	std::pair<int, int> getWindowDimensions() { std::pair<int, int> dims; glfwGetWindowSize(m_window, &dims.first, &dims.second); return dims; }
 	
-	// add callbacks for mouse events
+	// add callbacks for mouse events. we don't have these for keyboard events because keyboard steps, mouse doesn't step. mouse isn't analogue like a keyboard.
 	void AddMouseEvent(std::function<void(float, float)> callback) { m_mouse_callbacks.push_back(std::move(callback)); }
+	// helper function to get all the callbacks for when the mouse moves.
 	const std::vector<std::function<void(float, float)>>& GetMouseCallbacks() const { return m_mouse_callbacks; }
-	
-	float last_mouseX = 0.0f;
-	float last_mouseY = 0.0f;
-	float mouseX = 0.0f;
-	float mouseY = 0.0f;
-	
+
+	// get the scroll offset from the last frame. useful for reading mouse scrolling.
 	void setScrollOffset(float yoffset) { m_scroll_offset_y = yoffset; }
 	float getScrollOffset() const { return m_scroll_offset_y; }
 	
 private:
+	// the actual GLFW window.
 	GLFWwindow* m_window = nullptr;
 
+	// array of callbacks that are executed when GLFW registers a mouse event.
 	std::vector<std::function<void(float, float)>> m_mouse_callbacks;
 	/* @params
 	 * key - the GLFW keycode
